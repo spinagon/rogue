88133f76828e8f04dd6a210f343f1bcaacf837cb
spinagon <flak.yar@gmail.com>
2026-02-17 16:20:01 +0300

    add door and key tiles

--- src/ui/render.py
+++ src/ui/render.py
@@ -13,10 +13,12 @@ TILE_MAP = {
     Tile.FLOOR: ".",
     Tile.CHARACTER: "@",
     Tile.CORRIDOR: "#",
+    Tile.DOOR: "+",
     Tile.ITEM_WEAPON: "/",
     Tile.ITEM_ELIXIR: "!",
     Tile.ITEM_SCROLL: "%",
     Tile.ITEM_FOOD: "*",
+    Tile.ITEM_KEY: "ъ",
     Tile.UNKNOWN: "?",
     Tile.STAIR: ">",
     Tile.MONSTER_ZOMBIE: "z",
@@ -42,17 +44,17 @@ def init(win):
     win.leaveok(True)
 
 
-def draw(stdscr, frame: Frame):
-    stdscr.erase()
+def draw(win, frame: Frame):
+    win.erase()
     for t in frame.tiles:
         char = TILE_MAP.get(t.tile, "?")
         color = TILE_COLORS.get(t.tile, 0)
-        stdscr.addstr(t.y, t.x, char, curses.color_pair(color))
+        win.addstr(t.y, t.x, char, curses.color_pair(color))
 
     # Draw status bar
-    height, width = stdscr.getmaxyx()
+    height, width = win.getmaxyx()
     status = f"Level: {frame.level} | HP: {frame.hp}/{frame.max_hp} | Treasure: {frame.treasure}"
-    stdscr.addstr(height - 1, 0, status[:width])
-    stdscr.addstr(height - 2, 0, frame.message[:width])
+    win.addstr(height - 1, 0, status[:width])
+    win.addstr(height - 2, 0, frame.message[:width])
 
-    stdscr.refresh()
+    win.refresh()
