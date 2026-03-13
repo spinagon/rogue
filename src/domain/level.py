0e143f776e4041e1b74b4ec116cbbf88f522aa2e
spinagon <flak.yar@gmail.com>
2026-03-13 11:15:35 +0300

    fix typo

--- src/domain/level.py
+++ src/domain/level.py
@@ -1,17 +1,18 @@
 from domain.level_parts.room import Room
 
+
 class Level:
     ROOM_COUNT = 9
 
-    def __init__(self, difficult: float):
-        self.rooms = self._generate_rooms(difficult)
+    def __init__(self, difficulty: float):
+        self.rooms = self._generate_rooms(difficulty)
         self.corridors = self._generate_corridors_by_rooms()
 
-    def _generate_rooms(self, difficult: float):
+    def _generate_rooms(self, difficulty: float):
         rooms = []
 
         for i in range(self.ROOM_COUNT):
-            rooms.append(Room(difficult))
+            rooms.append(Room(difficulty))
 
         return rooms
 
